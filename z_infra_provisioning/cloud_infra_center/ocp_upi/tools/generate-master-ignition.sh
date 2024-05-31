0c03799d69071100a4e465b815507cea029196b8
Zheng Xiao Mei <xmzheng@cn.ibm.com>
2024-05-31 10:08:41 +0800

    Create node with sepcific name.
    Signed-off-by: Zheng Xiao Mei <xmzheng@cn.ibm.com>

--- z_infra_provisioning/cloud_infra_center/ocp_upi/tools/generate-master-ignition.sh
+++ z_infra_provisioning/cloud_infra_center/ocp_upi/tools/generate-master-ignition.sh
@@ -10,9 +10,16 @@
 
 infra_id=$1
 master_end=$(($2 - 1))
+name_prefix=$3
 
 for index in $( seq 0 $master_end); do
-    MASTER_HOSTNAME="$infra_id-master-$index\n"
+    if [ -n "$name_prefix" ]; then
+        MASTER_HOSTNAME="$name_prefix-$index\n"
+        IGNITION_NAME="$name_prefix-$index"
+    else
+        MASTER_HOSTNAME="$infra_id-master-$index\n"
+        IGNITION_NAME="$infra_id-master-$index"
+    fi
     python -c "import base64, json, sys
 ignition = json.load(sys.stdin)
 storage = ignition.get('storage', {})
@@ -20,5 +27,5 @@ files = storage.get('files', [])
 files.append({'path': '/etc/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$MASTER_HOSTNAME').decode().strip()},'filesystem': 'root'})
 storage['files'] = files
 ignition['storage'] = storage
-json.dump(ignition, sys.stdout)" < master.ign > "$infra_id-master-$index-ignition.json"
+json.dump(ignition, sys.stdout)" < master.ign > "$IGNITION_NAME-ignition.json"
 done
