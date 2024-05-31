e26d871dd1754498f3d2c66619b9f049ee93117c
Zheng Xiao Mei <xmzheng@cn.ibm.com>
2024-05-31 10:08:41 +0800

    Create node with sepcific name.
    Signed-off-by: Zheng Xiao Mei <xmzheng@cn.ibm.com>

--- z_infra_provisioning/cloud_infra_center/ocp_upi/tools/generate-bootstrap-ignitionshim.py
+++ z_infra_provisioning/cloud_infra_center/ocp_upi/tools/generate-bootstrap-ignitionshim.py
@@ -54,6 +54,22 @@ if ca_cert_path:
       }
     })
 
+name_prefix = sys.argv[4]
+if name_prefix:
+    name_prefix_byte = name_prefix.encode()
+    bootstrap_hostname = base64.standard_b64encode(name_prefix_byte).decode().strip()
+    files.update(
+    {
+      "storage": {
+        "files": {
+          "path": "/etc/hostname", 
+          "mode": 420, 
+          "contents": {
+              "source": "data:text/plain;charset=utf-8;base64," + bootstrap_hostname
+          }
+        }    
+      }
+    })
 infra_id = sys.argv[3]
 if infra_id:
     with open(infra_id+'-bootstrap-ignition.json', 'a') as f:
