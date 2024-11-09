095b1d3a2451e17fbfff92afdc5b7b6ec524313e
Bryant Panyarachun <bpanyarachun@gmail.com>
2024-11-08 16:52:35 -0800

    change delimiter from @@@ to 000.
    Signed-off-by: Bryant Panyarachun <bpanyarachun@gmail.com>

--- ibm_concert/cert_renewal/servicenow_script.js
+++ ibm_concert/cert_renewal/servicenow_script.js
@@ -1,7 +1,7 @@
 // Add extra_vars for Ansible template
 r.setRequestHeader('Content-Type', 'application/json');
 var obj = {};
-var new_desc = current.number + "@@@" + current.short_description;
+var new_desc = current.number + "000" + current.short_description;
 obj['sn_short_desc'] = new_desc;
 var final_obj = {};
 final_obj['extra_vars']= obj;
